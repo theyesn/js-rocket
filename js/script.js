9496c5b033100739b7818a510fb8ebeda2271bf7
theyesn <y.el.alaoui@live.nl>
2020-01-29 23:02:41 +0100

    Rocket man!!

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+}
+
+var car = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: [
+				'seat 1', 
+				'seat 2',
+				'seat 3',
+				'seat 4'
+			],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {console.log('turn car ' +isOn)
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				} else {
+					this.isTurnedOn = false;
+				}
+			}
+		};
+
+		console.log('hello there friends');
